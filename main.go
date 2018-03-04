17c2fcb2758153fa1d2dddd2857f6b4d7865ce77
Pierre Massat <pierre@massat.io>
2018-03-04 21:56:18 +0000

    Back to basics

--- main.go
+++ main.go
@@ -1,35 +1,23 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
 	"log"
 	"os/exec"
-	"time"
 )
 
 func main() {
 	var out bytes.Buffer
 
-	scanner := bufio.NewScanner(&out)
+	cmd := exec.Command("nvidia-smi", "--query-gpu=utilization.gpu", "--format=csv,noheader,nounits")
+	cmd.Stdout = &out
 
-	go func() {
-		for range time.Tick(time.Second) {
-			cmd := exec.Command("nvidia-smi", "--query-gpu=utilization.gpu", "--format=csv,noheader,nounits")
-			cmd.Stdout = &out
+	err := cmd.Run()
 
-			err := cmd.Run()
-
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}()
-
-	for {
-		for scanner.Scan() {
-			fmt.Println(scanner.Text())
-		}
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	fmt.Println(out.String())
 }
