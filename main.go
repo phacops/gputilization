b00aed833d9c995601e7bb83aedb4b2c3773c9de
Pierre Massat <pierre@massat.io>
2018-03-05 03:34:37 +0000

    Reduce height again

--- main.go
+++ main.go
@@ -45,9 +45,7 @@ func main() {
 	lc.Data = func() []float64 { return over }()
 	lc.AxesColor = termui.ColorWhite
 	lc.LineColor = termui.ColorCyan | termui.AttrBold
-	lc.Height = 128
-	lc.X = 300
-	lc.Y = 100
+	lc.Height = 100
 
 	termui.Body.AddRows(
 		termui.NewRow(
